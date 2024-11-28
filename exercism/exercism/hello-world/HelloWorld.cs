5abecae550999e9173a5cb28888234c3518b1da8
maiconVegas <maiconaruquipa@gmail.com>
2024-11-28 15:14:42 -0300

    adicionando gitignore e projeto de exercism

---
+++ exercism/exercism/hello-world/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.hello_world
+{
+    public class HelloWorld
+    {
+        public static string Hello() => "Hello, World!";
+    }
+}
---
+++ exercism/exercism/lucians-luscious-lasagna/Lasagna.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.lucians_luscious_lasagna
+{
+    public class Lasagna
+    {
+        // TODO: define the 'ExpectedMinutesInOven()' method
+        public int ExpectedMinutesInOven()
+        {
+            return 40;
+        }
+
+        // TODO: define the 'RemainingMinutesInOven()' method
+        public int RemainingMinutesInOven(int minute)
+        {
+            return ExpectedMinutesInOven() - minute;
+        }
+
+        // TODO: define the 'PreparationTimeInMinutes()' method
+        public int PreparationTimeInMinutes(int value)
+        {
+            return value * 2;
+        }
+
+        // TODO: define the 'ElapsedTimeInMinutes()' method
+        public int ElapsedTimeInMinutes(int value, int minute)
+        {
+            return PreparationTimeInMinutes(value) + minute;
+        }
+    }
+}
