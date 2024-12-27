28fee47f2e2cf0baeddbe1e21b66a1e83fba9348
maiconVegas <maiconaruquipa@gmail.com>
2024-12-27 17:10:05 -0300

    adicionando exercicio sobre integral numbers

---
+++ exercism/exercism/hyper-optimized-telemetry/TelemetryBuffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.hyper_optimized_telemetry
+{
+    public static class TelemetryBuffer
+    {
+        public static byte[] ToBuffer(long reading)
+        {
+            ulong ui = (ulong)reading;
+            return BitConverter.GetBytes(ui);
+        }
+
+        public static long FromBuffer(byte[] buffer)
+        {
+            throw new NotImplementedException("Please implement the static TelemetryBuffer.FromBuffer() method");
+        }
+    }
+}
