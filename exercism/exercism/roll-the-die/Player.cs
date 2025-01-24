16fe41a2cc0faf851c0ba3e4ce3114b255930591
maiconVegas <maiconaruquipa@gmail.com>
2025-01-24 17:22:14 -0300

    finalizando o exercicio legal de roll-the-die

---
+++ exercism/exercism/roll-the-die/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.roll_the_die
+{
+    internal class Player
+    {
+        public int RollDie() => new Random().Next(1, 18);
+        public double GenerateSpellStrength() => new Random().NextDouble()*100;
+    }
+}
