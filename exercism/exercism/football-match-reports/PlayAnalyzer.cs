4f1386f39da3b9beb7e9fae1967d5694e83e6476
maiconVegas <maiconaruquipa@gmail.com>
2025-01-27 15:11:37 -0300

    finalizando exercicio football_match_reports

---
+++ exercism/exercism/football-match-reports/PlayAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.football_match_reports;
+public static class PlayAnalyzer
+{
+    public static string AnalyzeOnField(int shirtNum) =>
+        shirtNum switch
+        {
+            1 => "goalie",
+            2 => "left back",
+            3 or 4 => "center back",
+            5 => "right back",
+            6 or 7 or 8 => "midfielder",
+            9 => "left wing",
+            10 => "striker",
+            11 => "right wing",
+            _ => "UNKNOWN"
+        };
+
+    public static string AnalyzeOffField(object report) =>
+        report switch
+        {
+            int => $"There are {report} supporters at the match.",
+            string => $"{report}",
+            Injury injury => $"Oh no! {injury.GetDescription()} Medics are on the field.",
+            Incident incident => incident.GetDescription(),
+            Manager manager when manager.Club == null => manager.Name,
+            Manager manager => $"{manager.Name} ({manager.Club})",
+            _ => string.Empty
+        };
+
+    
+}
+
+public class Manager
+{
+    public string Name { get; }
+
+    public string? Club { get; }
+
+    public Manager(string name, string? club)
+    {
+        this.Name = name;
+        this.Club = club;
+    }
+}
+
+public class Incident
+{
+    public virtual string GetDescription() => "An incident happened.";
+}
+
+public class Foul : Incident
+{
+    public override string GetDescription() => "The referee deemed a foul.";
+}
+
+public class Injury : Incident
+{
+    private readonly int player;
+
+    public Injury(int player)
+    {
+        this.player = player;
+    }
+
+    public override string GetDescription() => $"Player {player} is injured.";
+}
