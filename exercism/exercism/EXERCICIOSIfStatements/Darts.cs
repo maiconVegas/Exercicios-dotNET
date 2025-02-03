7170421de5ded5096bc79e7a5db797285cffae9a
maiconVegas <maiconaruquipa@gmail.com>
2025-02-03 15:51:16 -0300

    finalizando exercicio Darts

---
+++ exercism/exercism/EXERCICIOSIfStatements/Darts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.EXERCICIOSIfStatements;
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        var radius = Math.Sqrt(x * x + y * y);
+        if (radius <= 1)
+        {
+            return 10;
+        }
+        else if (radius <= 5)
+        {
+            return 5;
+        }
+        else if (radius <= 10)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
