96bcc5bccb351f5a3fa2eba5749a059607b8b54b
maiconVegas <maiconaruquipa@gmail.com>
2025-02-03 15:07:47 -0300

    adicionando novo exercicio tuple mds do ceu que exercicio vai ser esse? wordsearch

---
+++ exercism/exercism/EXERCICIOTuples/word-search/WordSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.EXERCICIOTuples.word_search
+{
+    public class WordSearch
+    {
+        private readonly string[] grid;
+        public WordSearch(string grid) => this.grid = grid.Split("\n");
+
+        public Dictionary<string, ((int, int), (int, int))?> Search(string[] wordsToSearchFor)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
