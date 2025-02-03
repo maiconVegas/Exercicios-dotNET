5b081a16e7989cd251d1ec05e9d12d053fcb2305
maiconVegas <maiconaruquipa@gmail.com>
2025-02-03 15:22:31 -0300

    finalizando exercicio Leap

--- exercism/exercism/EXERCICIOSIfStatements/AssemblyLine.cs
+++ exercism/exercism/EXERCICIOSIfStatements/AssemblyLine.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace exercism.IfStatements
+namespace exercism.EXERCICIOSIfStatements
 {
     public class AssemblyLine
     {
---
+++ exercism/exercism/EXERCICIOSIfStatements/Leap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.EXERCICIOSIfStatements
+{
+    public static class Leap
+    {
+        public static bool IsLeapYear(int year) =>
+            (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
