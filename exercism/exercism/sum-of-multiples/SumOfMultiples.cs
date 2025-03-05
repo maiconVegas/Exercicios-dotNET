f086a4655b670cac4ceb2856764508a4321e7721
Maicon Lipa <maiconaruquipa@gmail.com>
2025-03-05 17:12:05 -0300

    resovendo sum-of-multiples

---
+++ exercism/exercism/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercism.sum_of_multiples;
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int sum = 0;
+        for (int i = 1; i < max; i++)
+        {
+            foreach (int m in multiples)
+            {
+                if (m != 0 && i % m == 0)
+                {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
+}
